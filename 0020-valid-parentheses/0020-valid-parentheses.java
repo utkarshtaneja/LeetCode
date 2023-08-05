4681cbca10f4c70be10356203944a704cad4a0c6
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-08-05 23:42:07 +0530

    Time: 1 ms (98.89%), Space: 40.4 MB (89.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else {
+                if (st.isEmpty()) {
+                    return false; 
+                }
+
+                char top = st.pop();
+                if ( (ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{') ) {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
