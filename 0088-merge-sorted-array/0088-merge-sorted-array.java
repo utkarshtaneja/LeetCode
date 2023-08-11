f5ec928af9a458e1a911aa104d8f5b666866ba58
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-08-11 18:04:16 +0530

    Time: 1 ms (37.74%), Space: 41.4 MB (41.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,5 +4,27 @@ class Solution {
             nums1[m+i] = nums2[i];
         }
         Arrays.sort(nums1);
+
+        // OPTIMAL SOLUTION
+        // int i = m - 1; 
+        // int j = n - 1; 
+        // int k = m + n - 1; 
+        
+        // while (i >= 0 && j >= 0) {
+        //     if (nums1[i] > nums2[j]) {
+        //         nums1[k] = nums1[i];
+        //         i--;
+        //     } else {
+        //         nums1[k] = nums2[j];
+        //         j--;
+        //     }
+        //     k--;
+        // }
+        
+        // while (j >= 0) {
+        //     nums1[k] = nums2[j];
+        //     j--;
+        //     k--;
+        // }
     }
 }
