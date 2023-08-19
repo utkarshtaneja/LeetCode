7ca24bac44ef5881c6367a189b972cf27de82544
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-08-19 16:47:56 +0530

    Time: 0 ms (100%), Space: 40.6 MB (83.5%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,12 +1,34 @@
 class Solution {
     public int findMin(int[] nums) {
         // BRUTE FORCE METHOD
-        int min = Integer.MAX_VALUE;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] < min){
-                min = nums[i];
+        // int min = Integer.MAX_VALUE;
+        // for(int i = 0;i<nums.length;i++){
+        //     if(nums[i] < min){
+        //         min = nums[i];
+        //     }
+        // }
+        // return min;
+
+        // OPTIMAL APPROACH
+        int low = 0;
+        int high = nums.length-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+
+            if(nums[low] <= nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid + 1;
+            }
+
+            else{
+                ans = Math.min(ans,nums[mid]);
+                high = mid - 1;
             }
         }
-        return min;
+
+        return ans;
     }
 }
