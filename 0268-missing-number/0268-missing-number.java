a0ad6764ed82b35693cafe931e5afc1ac9f6ec54
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-08-25 14:02:00 +0530

    Time: 0 ms (100%), Space: 43.7 MB (96.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 1;i<=n;i++){
+            sum = sum + i;
+        }
+
+        for(int i = 0;i<n;i++){
+            sum = sum - nums[i];
+        }
+
+        return sum;
+    }
+}
