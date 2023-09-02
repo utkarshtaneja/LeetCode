05a7644059a92364a1e683e230603f15df59353a
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-02 19:40:20 +0530

    Time: 10 ms (83.83%), Space: 45.4 MB (22.96%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -15,12 +15,14 @@ class Solution {
         return days;
     }
     public int shipWithinDays(int[] weights, int days) {
-        int low = Integer.MIN_VALUE, high = 0;
+        int low = 0;
+        int high = 0;
         for (int i = 0; i < weights.length; i++) {
-            high += weights[i];
-            low = Math.max(low, weights[i]);
+            high = high + weights[i];// summation
+            low = Math.max(low, weights[i]);// max value
         }
 
+        // Range = (maxValue , summation)
         while (low <= high) {
             int mid = (low + high) / 2;
             int numberOfDays = findDays(weights, mid);
