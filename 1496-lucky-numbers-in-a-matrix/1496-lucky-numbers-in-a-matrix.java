ffb2df30a0e10c7d36221aa95e0e32b957a6cfd0
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-07 14:40:23 +0530

    Time: 2 ms (85.72%), Space: 44.4 MB (43.38%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -2,46 +2,32 @@ class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-        List<Integer> luckyNumbers = new ArrayList<>();
+        
+        List<Integer> ans = new ArrayList<>();
         
         for(int i = 0; i < row; i++) {
             int minInRow = Integer.MAX_VALUE;
-            int minColIndex = -1;
+            int index = -1;
             for(int j = 0; j < col; j++) {
-                if (matrix[i][j] < minInRow) {
+                if(matrix[i][j] < minInRow) {
                     minInRow = matrix[i][j];
-                    minColIndex = j;
+                    index = j;
                 }
             }
-            boolean isMaxInCol = true;
+            boolean isMaximum = true;
             for(int k = 0; k < row; k++) {
-                if(matrix[k][minColIndex] > minInRow) {
-                    isMaxInCol = false;
+                if(matrix[k][index] > minInRow) {
+                    isMaximum = false;
                     break;
                 }
             }
-            if(isMaxInCol) {
-                luckyNumbers.add(minInRow);
+            
+            if(isMaximum) {
+                ans.add(minInRow);
             }
         }
-        return luckyNumbers;
+        
+        return ans;
     }
 }
 
-// class Solution {
-//     public List<Integer> luckyNumbers (int[][] matrix) {
-//         int max = Integer.MIN_VALUE;
-//         int row = matrix.length;
-//         int col = matrix[0].length;
-//         for(int i = 0;i<row;i++){
-//             int min = Integer.MAX_VALUE;
-//             for(int j = 0;j<col;j++){
-//                 min = Math.min(min,matrix[i][j]);
-//             }
-//             max = Math.max(max,min);
-//         }
-//         List<Integer> list = new ArrayList<>();
-//         list.add(max);
-//         return list;
-//     }
-// }
