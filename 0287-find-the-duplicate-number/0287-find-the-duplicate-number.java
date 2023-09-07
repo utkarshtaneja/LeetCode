8960f4d5636b3fede85fd223c432361dc897d980
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-07 13:58:52 +0530

    Time: 4 ms (87.66%), Space: 56.8 MB (31.99%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,23 +1,41 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                int incr = map.getOrDefault(nums[i],1)+1;
-                map.put(nums[i],incr);
-            }
-            else{
-                map.put(nums[i],1);
-            }
-        }
+        // MY SOLUTION
+        // int n = nums.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i = 0;i<n;i++){
+        //     if(map.containsKey(nums[i])){
+        //         int incr = map.getOrDefault(nums[i],1)+1;
+        //         map.put(nums[i],incr);
+        //     }
+        //     else{
+        //         map.put(nums[i],1);
+        //     }
+        // }
 
-        int ans = -1;
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue() > 1){
-                ans = e.getKey();
-            }
+        // int ans = -1;
+        // for(Map.Entry<Integer,Integer> e : map.entrySet()){
+        //     if(e.getValue() > 1){
+        //         ans = e.getKey();
+        //     }
+        // }
+        // return ans;
+
+        // OPTIMIZED METHOD - Tortoise approach
+        int n = nums.length;
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return ans;
+        return slow;
     }
 }
