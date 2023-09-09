b6f6947f2cae837fce775bf0705b2b56631bab95
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-09 18:39:52 +0530

    Time: 0 ms (100%), Space: 41 MB (63.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,22 +1,45 @@
-class Solution {
-    public static int nCr(int n,int r){
-        int result = 1;
-        for(int i = 0;i<r;i++){
-            result = result * (n-i);
-            result = result/(i+1);
+class Solution{
+    public static List<Integer> generateRow(int row){
+        int ans = 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        for(int col = 1;col<row;col++){
+            ans = ans * (row - col);
+            ans = ans / col;
+            list.add(ans);
         }
-        return result;
+
+        return list;
     }
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int numRows){
         List<List<Integer>> list = new ArrayList<>();
         int result = 1;
-        for(int i = 0;i<numRows;i++){
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j <= i; j++) {
-                row.add(nCr(i, j));
-            }
-            list.add(row);
+        for(int i = 1;i<=numRows;i++){
+            list.add(generateRow(i));
         }
         return list;
     }
 }
+// class Solution {
+//     public static int nCr(int n,int r){
+//         int result = 1;
+//         for(int i = 0;i<r;i++){
+//             result = result * (n-i);
+//             result = result/(i+1);
+//         }
+//         return result;
+//     }
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         int result = 1;
+//         for(int i = 0;i<numRows;i++){
+//             List<Integer> row = new ArrayList<>();
+//             for(int j = 0; j <= i; j++) {
+//                 row.add(nCr(i, j));
+//             }
+//             list.add(row);
+//         }
+//         return list;
+//     }
+// }
