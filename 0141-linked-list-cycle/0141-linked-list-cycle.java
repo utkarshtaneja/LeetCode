17e6e9a236cabab1e585423138abff96755cf4e3
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-13 13:11:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (93.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,32 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while(head != null) {
-            if(set.contains(head)){
+        // Brute Force Method
+        // Set<ListNode> set = new HashSet<>();
+        // while(head != null) {
+        //     if(set.contains(head)){
+        //         return true;
+        //     }
+        //     set.add(head);
+        //     head = head.next;
+        // }
+        // return false;
+
+
+        // Optimal Approach
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        slow = slow.next;
+        fast = fast.next.next;
+        while(fast != null && fast.next != null){
+            if(slow == fast){
                 return true;
             }
-            set.add(head);
-            head = head.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return false;
     }
