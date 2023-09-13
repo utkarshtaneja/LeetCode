b681bd861cd06bdecf9df7c379c74cd4c2021aa3
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-09-13 13:13:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (21.37%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -29,14 +29,12 @@ public class Solution {
         }
         ListNode slow = head;
         ListNode fast = head;
-        slow = slow.next;
-        fast = fast.next.next;
         while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
             if(slow == fast){
                 return true;
             }
-            slow = slow.next;
-            fast = fast.next.next;
         }
         return false;
     }
