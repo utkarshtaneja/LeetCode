5d6b409f336515197913231487bb1f6bb68d0513
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-10-09 18:46:19 +0530

    Time: 3 ms (89.32%), Space: 42.3 MB (88.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -7,9 +7,9 @@ class Solution {
         boolean flag = false;
         for(int i = 0; i < n1; i++) {
             ans[i] = -1;
-            for (int j = 0; j < n2; j++) {
+            for(int j = 0; j < n2; j++) {
                 if(nums1[i] == nums2[j]) {
-                    for (int k = j + 1; k < n2; k++) {
+                    for(int k = j + 1; k < n2; k++) {
                         if(nums2[k] > nums2[j]) {
                             ans[i] = nums2[k];
                             break;
