915962231328588365ee5e1d89227e13477ef4c8
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-10-10 13:54:21 +0530

    Time: 0 ms (100%), Space: 41.7 MB (15.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,23 +11,24 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         // Iterative Method
-        // ListNode newHead = null;
-        // while(head != null){
-        //     ListNode nxt = head.next;
-        //     head.next = newHead;
-        //     newHead = head;
-        //     head = nxt;
-        // }
-        // return newHead;
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
 
         // Recursive Method
-        if(head == null || head.next == null){
-            return head;
-        }
+        // if(head == null || head.next == null){
+        //     return head;
+        // }
 
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
+        // ListNode newHead = reverseList(head.next);
+        // head.next.next = head;
+        // head.next = null;
+        // return newHead;
     }
 }
