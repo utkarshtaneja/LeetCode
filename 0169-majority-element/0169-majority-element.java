c99aedbff4b42b4f975b096fa096e82d9dbfdd9e
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-03 12:59:48 +0530

    Time: 2 ms (66.77%), Space: 49 MB (8.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,47 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // Brute Force 
+        int count = 1;
+        int candidate = nums[0];
         int n = nums.length;
-        for(int i = 0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                int incr = map.getOrDefault(nums[i],1)+1;
-                map.put(nums[i],incr);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == candidate) {
+                count++;
+            } 
+            else {
+                count--;
+                if(count == 0) {
+                    candidate = nums[i];
+                    count = 1;
+                }
             }
-            else{
-                map.put(nums[i],1);
+        }
+        count = 0;
+        for(int num : nums) {
+            if(num == candidate) {
+                count++;
             }
         }
 
-        int ans = 0;
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue() > n/2){
-                ans = e.getKey();
-            }
-        }
-
-        return ans;
-
+        return count > n / 2 ? candidate : -1;
+        // Optimal Approach
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // int n = nums.length;
+        // for(int i = 0;i < n;i++){
+        //     if(map.containsKey(nums[i])){
+        //         int incr = map.getOrDefault(nums[i],0)+1;
+        //         map.put(nums[i],incr);
+        //     }
+        //     else{
+        //         map.put(nums[i],1);
+        //     }
+        // }
+        // int max = 0;
+        // for(Map.Entry<Integer,Integer> e : map.entrySet()){
+        //     if(e.getValue() > n/2){
+        //         max = e.getKey();
+        //     }
+        // }
+        // return max;
     }
 }
