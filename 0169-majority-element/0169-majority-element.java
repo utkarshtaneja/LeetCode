0c5c61770997ba93927c237725d29261c7ea0eb9
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-03 13:00:49 +0530

    Time: 13 ms (24.37%), Space: 46.9 MB (79.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,47 +1,47 @@
 class Solution {
     public int majorityElement(int[] nums) {
         // Brute Force 
-        int count = 1;
-        int candidate = nums[0];
-        int n = nums.length;
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] == candidate) {
-                count++;
-            } 
-            else {
-                count--;
-                if(count == 0) {
-                    candidate = nums[i];
-                    count = 1;
-                }
-            }
-        }
-        count = 0;
-        for(int num : nums) {
-            if(num == candidate) {
-                count++;
-            }
-        }
+//         int count = 1;
+//         int candidate = nums[0];
+//         int n = nums.length;
+//         for(int i = 1; i < nums.length; i++) {
+//             if(nums[i] == candidate) {
+//                 count++;
+//             } 
+//             else {
+//                 count--;
+//                 if(count == 0) {
+//                     candidate = nums[i];
+//                     count = 1;
+//                 }
+//             }
+//         }
+//         count = 0;
+//         for(int num : nums) {
+//             if(num == candidate) {
+//                 count++;
+//             }
+//         }
 
-        return count > n / 2 ? candidate : -1;
+//         return count > n / 2 ? candidate : -1;
         // Optimal Approach
-        // HashMap<Integer,Integer> map = new HashMap<>();
-        // int n = nums.length;
-        // for(int i = 0;i < n;i++){
-        //     if(map.containsKey(nums[i])){
-        //         int incr = map.getOrDefault(nums[i],0)+1;
-        //         map.put(nums[i],incr);
-        //     }
-        //     else{
-        //         map.put(nums[i],1);
-        //     }
-        // }
-        // int max = 0;
-        // for(Map.Entry<Integer,Integer> e : map.entrySet()){
-        //     if(e.getValue() > n/2){
-        //         max = e.getKey();
-        //     }
-        // }
-        // return max;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0;i < n;i++){
+            if(map.containsKey(nums[i])){
+                int incr = map.getOrDefault(nums[i],0)+1;
+                map.put(nums[i],incr);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        int max = 0;
+        for(Map.Entry<Integer,Integer> e : map.entrySet()){
+            if(e.getValue() > n/2){
+                max = e.getKey();
+            }
+        }
+        return max;
     }
 }
