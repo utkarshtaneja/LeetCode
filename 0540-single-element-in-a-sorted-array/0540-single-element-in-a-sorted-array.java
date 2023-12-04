8e5817ce1c298c6e9aa76385b723e8cf741252e0
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-04 14:24:47 +0530

    Time: 0 ms (100.00%), Space: 50 MB (39.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,45 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         // Brute Force
-        int ans = 0;
-        for(int i = 0;i < nums.length;i++){
-            ans = ans ^ nums[i];
+        // int ans = 0;
+        // for(int i = 0;i < nums.length;i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
+        
+        // Optimal Approach        
+        int n = nums.length;       
+        if(n == 1){
+            return nums[0];
         }
-        return ans;
+        
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        
+        if(nums[n-1] != nums[n-2]){
+            return nums[n-1];
+        }
+        
+        int low = 1;
+        int high = n-2;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            else if( (mid % 2 == 1 && nums[mid-1] == nums[mid] ) || 
+                     (mid % 2 == 0 && nums[mid] == nums[mid+1] ) ){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 }
+// Play on even and odd index
+// (even, odd) -> left half    ---> element is on right half
+// (odd, even) -> right half   ---> element is on left half
