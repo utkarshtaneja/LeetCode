4b416638c7e8b65f7906cbe194381083d74a9098
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-05 02:21:11 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (12.10%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,32 +1,12 @@
 class Solution {
-    public static boolean check(int[] nums){
-        for(int i = 0;i < nums.length - 1;i++){
-            if(nums[i] > nums[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static boolean rev(int[] nums){
-        for(int i = 0;i < nums.length - 1;i++){
-            if(nums[i] < nums[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
     public int findPeakElement(int[] nums) {
         // Brute Force Method
         int n = nums.length;
         if(n == 1) return 0;
-        
-        boolean checkSorted = check(nums);
-        if(checkSorted) return n-1;
-        
-        boolean reverse = rev(nums);
-        if(reverse) return 0;
-        for(int i = 1;i < n-1;i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+
+        for(int i = 0;i < n;i++){
+            if( (i == 0 || nums[i-1] < nums[i]) &&
+                (i == n-1) || nums[i] > nums[i+1]){
                 return i;
             }
         }
