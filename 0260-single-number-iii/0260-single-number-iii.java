06eccf46f4cb9b838e3dcf583d8bb2fd81ea7bdb
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-16 21:19:42 +0530

    Time: 6 ms (33.82%), Space: 44 MB (87.83%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(nums[i])) {
-                int incr = map.get(nums[i]);
-                map.put(nums[i], incr + 1);
-            } 
-            else {
-                map.put(nums[i], 1);
+         HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int incr = map.getOrDefault(nums[i],0) + 1;
+                map.put(nums[i],incr);
+            }
+            else{
+                map.put(nums[i],1);
             }
         }
-
-        int[] result = new int[2];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                result[i] = entry.getKey();
-                i++;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(Map.Entry<Integer,Integer> e : map.entrySet()){
+            if(e.getValue() == 1){
+                list.add(e.getKey());
             }
         }
-
-        return result;
+        int[] ans = new int[list.size()];
+        for(int i = 0;i < list.size();i++){
+            ans[i] = list.get(i);
+        }
+        return ans;  
     }
 }
