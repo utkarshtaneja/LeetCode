a803657dae88913db00a2293b32f0b7f58ecdbe4
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-29 12:12:10 +0530

    Time: 1 ms (19.18%), Space: 43.3 MB (6.52%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,20 +13,51 @@
  *     }
  * }
  */
+// class Solution {
+//     public static int height(TreeNode root){
+//         if(root == null) return 0;
+//         int leftDepth = height(root.left);
+//         int rightDepth = height(root.right);
+//         int ans = Math.max(leftDepth,rightDepth) + 1;
+//         return ans;
+//     }
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root == null) return 0;
+//         int Ls = diameterOfBinaryTree(root.left);
+//         int Rs = diameterOfBinaryTree(root.right);
+//         int diam = height(root.left) + height(root.right) ;
+//         int ans = Math.max(diam,Math.max(Ls,Rs));
+//         return ans;
+//     }
+// }
+
+// Optimal Approach
 class Solution {
-    public static int height(TreeNode root){
-        if(root == null) return 0;
-        int leftDepth = height(root.left);
-        int rightDepth = height(root.right);
-        int ans = Math.max(leftDepth,rightDepth) + 1;
-        return ans;
+    static class TreeInfo{
+        int height;
+        int diameter;
+        
+        TreeInfo(int height,int diameter){
+            this.height = height;
+            this.diameter = diameter;
+        }
+    }
+    public static TreeInfo diameterOfTree(TreeNode root){
+        if(root == null) return new TreeInfo(0,0); 
+        TreeInfo left = diameterOfTree(root.left);
+        TreeInfo right = diameterOfTree(root.right);
+        
+        int myHeight = Math.max(left.height,right.height) + 1;
+        int diam1 = left.diameter;
+        int diam2 = right.diameter;
+        int diam3 = left.height + right.height ;
+        
+        int mydiam = Math.max(Math.max(diam1,diam2),diam3);
+        TreeInfo myInfo = new TreeInfo(myHeight, mydiam);
+        return myInfo;
     }
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        int Ls = diameterOfBinaryTree(root.left);
-        int Rs = diameterOfBinaryTree(root.right);
-        int diam = height(root.left) + height(root.right) ;
-        int ans = Math.max(diam,Math.max(Ls,Rs));
-        return ans;
+        TreeInfo info = diameterOfTree(root);
+        return info.diameter;
     }
 }
