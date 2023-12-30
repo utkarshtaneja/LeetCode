2df23cea791b9f47a342ae54d671d2360b94a888
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2023-12-30 12:30:46 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (12.79%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -20,32 +20,32 @@ class Solution {
         }
         return root;
     }
-
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null) {
             return null;
         }
-
+        
         if(root.val > key){
-            root.left = deleteNode(root.left, key);
+            root.left = deleteNode(root.left,key);
         }
         else if(root.val < key){
-            root.right = deleteNode(root.right, key);
+            root.right = deleteNode(root.right,key);
         }
-        else {
+        else{
             // case 1 - leaf node
             if(root.left == null && root.right == null){
                 return null;
             }
-
+            
             // case 2 - 1 child
             if(root.left == null){
                 return root.right;
             }
+            
             else if(root.right == null){
                 return root.left;
             }
-
+            
             // case 3 - 2 child
             TreeNode IS = inorderSuccessor(root.right);
             root.val = IS.val;
