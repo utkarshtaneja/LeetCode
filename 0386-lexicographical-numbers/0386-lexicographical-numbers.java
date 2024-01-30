1bd2c847acb682459592ccdc0e798ebb87e059c7
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-01-30 13:27:10 +0530

    Time: 1 ms (99.90%), Space: 47.9 MB (73.51%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,11 +1,32 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         // Brute Force
-        List<String> list = new ArrayList<>();
-        for(int i = 1;i <= n;i++){
-            list.add(String.valueOf(i));
+        // List<String> list = new ArrayList<>();
+        // for(int i = 1;i <= n;i++){
+        //     list.add(String.valueOf(i));
+        // }
+        // Collections.sort(list);
+        // return list.stream().map(Integer::parseInt).collect(Collectors.toList());
+        
+        // optimal
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1;i < 10;i++){
+            dfs(i, n, list);
         }
-        Collections.sort(list);
-        return list.stream().map(Integer::parseInt).collect(Collectors.toList());
+        return list;
+    }
+    private static void dfs(int curr,int n,List<Integer> list){
+        if(curr > n){
+            return;
+        }
+        else{
+            list.add(curr);
+            for(int i = 0;i < 10;i++){
+                if(10 * curr + i > n){
+                    return;
+                }
+                dfs(10* curr + i ,n,list);
+            }
+        }
     }
 }
