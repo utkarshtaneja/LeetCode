21c2e652f49ac37f9a90b0a78a1b5eb3c5ce5168
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-02-16 14:37:10 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (94.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,55 +1,50 @@
 class Solution {
+    public static int first(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int last(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
     public int[] searchRange(int[] nums, int target) {
-        int s = first(nums,target);
-        if(s == -1){
-            return new int[]{-1,-1};
+        int start = first(nums, target);
+        if(start == -1){
+            return new int[]{-1, -1};
         }
-
-        int e = last(nums,target);
-        return new int[]{s,e};
-    }
-
-    private static int first(int[] nums,int target){
-        int s = 0;
-        int e = nums.length - 1;
-        int result = -1;
-
-        while(s<=e){
-            int mid = s + (e - s) / 2;
-            if(nums[mid] == target){
-                result = mid;
-                e = mid - 1;
-            }
-            else if(nums[mid] < target){
-                s = mid + 1;
-            }
-            else if(nums[mid] > target){
-                e = mid - 1;
-            }
-        }
-
-        return result;
-    }
-
-    private static int last(int[] nums,int target){
-        int s = 0;
-        int e = nums.length - 1;
-        int result = -1;
-
-        while(s<=e){
-            int mid = s + (e - s) / 2;
-            if(nums[mid] == target){
-                result = mid;
-                s = mid + 1;
-            }
-            else if(nums[mid] < target){
-                s = mid + 1;
-            }
-            else if(nums[mid] > target){
-                e = mid - 1;
-            }
-        }
-
-        return result;
+        int end = last(nums, target);
+        return new int[]{start, end};
     }
 }
