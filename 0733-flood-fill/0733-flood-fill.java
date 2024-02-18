01f635da442cac7de93e5a8ae322a0eb3a51a699
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-02-18 12:28:22 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (35.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,28 @@
 class Solution {
-    public static void dfs(int row,int col,int[][] ans,int[][] image,int color,int[] delRow,int[] delCol,int init){
+    public static void dfs(int row,int col,int[][] image, int[][] ans, int init, int color, int[] delRow,int[] delCol){
+        
         ans[row][col] = color;
-        int n = image.length;
-        int m = image[0].length;
+        int n = ans.length;
+        int m = ans[0].length;
         
         for(int i = 0;i < 4;i++){
+            
             int nRow = row + delRow[i];
             int nCol = col + delCol[i];
+            
             if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == init && ans[nRow][nCol] != color){
-                dfs(nRow, nCol, ans, image, color, delRow, delCol, init);
+                dfs(nRow, nCol, image, ans, init, color, delRow, delCol);
             }
-        } 
+        }
     }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color){
-        // Code here 
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int init = image[sr][sc];
         int[][] ans = image;
         
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1};
-        dfs(sr, sc, ans, image, color, delRow, delCol, init);
+        int[] delRow = {-1, 0, +1, 0};
+        int[] delCol = {0, +1, 0, -1};
+        
+        dfs(sr, sc, image, ans, init, color, delRow, delCol);
         return ans;
     }
 }
