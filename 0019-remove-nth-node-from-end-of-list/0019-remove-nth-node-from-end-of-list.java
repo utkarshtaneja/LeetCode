0d8bc8b9ae8c6b18e6423450543e06c3d45bec48
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-03-05 20:44:45 +0530

    Time: 0 ms (100.00%), Space: 41 MB (98.59%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,30 +9,77 @@
  * }
  */
 class Solution {
-    public static int length(ListNode head){
-        int count = 0;
-        while(head != null) {
-            count++;
-            head = head.next;
+    public static ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode temp = head;
+        
+        while(temp != null){
+            prev = curr;
+            curr = temp;
+            temp = temp.next;
+            curr.next = prev;
         }
-        return count;
+        return curr;
     }
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next == null){
-            return null;
-        }
-        int size = length(head);
-        if(n == size){
-            return head.next;
-        }
-        else{
-            int positionToRemove = size - n - 1;
-            ListNode temp = head;
-            for(int i = 0; i<positionToRemove; i++) {
-                temp = temp.next;
-            }
-            temp.next = temp.next.next;
+    public static ListNode remove(ListNode head, int n){
+        if(head == null || n <= 0) {
             return head;
         }
+        
+        if(n == 1) {
+            return head.next;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        for(int i = 0; i < n; i++) {
+            if(fast == null) {
+                return head;
+            }
+            fast = fast.next;
+        }
+        
+        if(fast == null) {
+            return head.next;
+        }
+        
+        while(fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || n <= 0) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for(int i = 0; i <= n; i++) {
+            if(fast == null) {
+                return head;
+            }
+            fast = fast.next;
+        }
+
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
     }
 }
