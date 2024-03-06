61ee4a17ecab0dc610ffae314d5a397dcef0d061
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-03-06 20:26:52 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (78.40%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,30 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // Brute Force Method
-        // Set<ListNode> set = new HashSet<>();
-        // while(head != null) {
-        //     if(set.contains(head)){
-        //         return true;
-        //     }
-        //     set.add(head);
-        //     head = head.next;
-        // }
-        // return false;
-
-
-        // Optimal Approach
         if(head == null || head.next == null){
             return false;
         }
         ListNode slow = head;
-        ListNode fast = head;
+        ListNode fast = head.next;
+        
         while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
             if(slow == fast){
                 return true;
             }
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return false;
     }
