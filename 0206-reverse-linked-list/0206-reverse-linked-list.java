9298a6c54ba073a9999d795c4f4976df247785ee
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-03-21 10:36:48 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (94.64%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,9 +10,12 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // Iterative Method
+        if(head == null || head.next == null){
+            return head;
+        }
         ListNode curr = head;
         ListNode prev = null;
+        
         while(curr != null){
             ListNode temp = curr.next;
             curr.next = prev;
@@ -20,15 +23,5 @@ class Solution {
             curr = temp;
         }
         return prev;
-
-        // Recursive Method
-        // if(head == null || head.next == null){
-        //     return head;
-        // }
-
-        // ListNode newHead = reverseList(head.next);
-        // head.next.next = head;
-        // head.next = null;
-        // return newHead;
     }
 }
