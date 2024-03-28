a6fd75de1d74202bb8c9a61fdb78997b64fbb291
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-03-28 08:16:46 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (68.46%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -10,34 +10,26 @@ class Solution {
     }
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-
-        // 1 Find the break point
-        int index = -1; 
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] < nums[i + 1]){
+        int index = -1;
+        
+        for(int i = n - 2;i >= 0;i--){
+            if(nums[i] < nums[i+1]){
                 index = i;
                 break;
             }
         }
-
-        // If break point does not exist
-        if(index == -1) {
-            // reverse the whole array
-            reverse(0, n - 1, nums);
-        } 
-
-        else {
-            //2: Find the next greater element and swap it with nums[index]
-            for(int i = n - 1; i > index; i--) {
-                if(nums[i] > nums[index]) {
+        if(index == -1){
+            reverse(0, n-1, nums);
+        }
+        else{
+            for(int i= n - 1;i > index;i--){
+                if(nums[i] > nums[index]){
                     int temp = nums[i];
                     nums[i] = nums[index];
                     nums[index] = temp;
                     break;
                 }
             }
-
-            // Step 3: reverse the right half:
             reverse(index + 1, n - 1, nums);
         }
     }
