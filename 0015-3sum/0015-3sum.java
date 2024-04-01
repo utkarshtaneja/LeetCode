d56be189ea48a459345bba2393867ef636daed19
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-04-01 12:33:11 +0530

    Time: 32 ms (56.55%), Space: 51.2 MB (77.08%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -44,37 +44,32 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            
-            if(i != 0 && nums[i] == nums[i - 1]){
-                continue;
-            }
-
-            int j = i + 1;
-            int k = n - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;
-                } 
-                else if(sum > 0) {
-                    k--;
-                } 
-                else {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j - 1]){
-                        j++;
+        
+        for(int i = 0;i < n -2;i++){
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                int s = i + 1;
+                int e = n - 1;
+                
+                while(s < e){
+                    int sum = nums[i] + nums[s] + nums[e];
+                    if(sum == 0){
+                        ans.add(Arrays.asList(nums[i], nums[s], nums[e]));
+                        
+                        while(s < e && nums[s] == nums[s+1]) s++;
+                        while(s < e && nums[e] == nums[e-1]) e--;
+                        
+                        s++;
+                        e--;
                     }
-                    while(j < k && nums[k] == nums[k + 1]){
-                        k--;
+                    else if(sum < 0){
+                        s++;
+                    }
+                    else{
+                        e--;
                     }
                 }
             }
         }
-
         return ans;
     }
 }
