18a316e72ab6ebe45da630b4bf35e6dce135adbd
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-06-12 18:11:01 +0530

    Time: 2 ms (73.27%), Space: 42.6 MB (25.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -15,30 +15,29 @@ class Solution {
         int m = grid[0].length;
         
         int[][] vis = new int[n][m];
+        int[][] ans = new int[n][m];
         int fresh = 0;
         
         Queue<Pair> q = new LinkedList<>();
         
         for(int i = 0;i < n;i++){
             for(int j = 0;j < m;j++){
-                if(grid[i][j] == 2){
-                    q.add(new Pair(i, j, 0));
-                    vis[i][j] = 2;
-                }
-                else if(grid[i][j] == 1){
+                if(grid[i][j] == 1){
+                    ans[i][j] = 1;
                     fresh++;
                 }
-                else{
-                    vis[i][j] = 0;
+                else if(grid[i][j] == 2){
+                    vis[i][j] = 1;
+                    q.add(new Pair(i, j, 0));
                 }
             }
         }
         
+        int maxTime = 0;
+        int count = 0;
+        
         int[] dRow = {-1, 0, 1, 0};
         int[] dCol = {0, 1, 0, -1};
-        
-        int max = 0;
-        int check = 0;
         
         while(!q.isEmpty()){
             Pair curr = q.poll();
@@ -46,21 +45,23 @@ class Solution {
             int col = curr.col;
             int time = curr.time;
             
-            max = Math.max(time, max);
+            maxTime = Math.max(time, maxTime);
             
             for(int i = 0;i < 4;i++){
                 int nRow = row + dRow[i];
                 int nCol = col + dCol[i];
                 
-                if(nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && vis[nRow][nCol] == 0 && grid[nRow][nCol] == 1){
-                    vis[nRow][nCol] = 2;
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0 && grid[nRow][nCol] != 0 && ans[nRow][nCol] == 1){
+                    vis[nRow][nCol] = 1;
+                    ans[nRow][nCol] = 2;
+                    count++;
                     q.add(new Pair(nRow, nCol, time + 1));
-                    check++;
                 }
             }
         }
         
-        if(check != fresh) return -1;
-        return max;
+        if(count != fresh) return -1;
+        
+        return maxTime;
     }
 }
