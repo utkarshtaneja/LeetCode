066f0a865a8027690c761a603cc246f4f9ea7021
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-13 12:23:35 +0530

    Time: 38 ms (42.82%), Space: 44.9 MB (78.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // BRUTE FORCE APPROACH
-        // int[] ans = new int[2];
-        // for (int i = 0; i < nums.length; i++) {
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         if (nums[i] + nums[j] == target) {
-        //             ans[0] = i;
-        //             ans[1] = j;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return ans;
-
-        // OPTIMIZED APPROACH
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int rem = target - num;
-            if (map.containsKey(rem)) {
-                return new int[]{i, map.get(rem)};
+        // Brute Force
+        int n = nums.length;
+        int[] ans = new int[2];
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < i;j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
             }
-            map.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+        return ans;
     }
 }
