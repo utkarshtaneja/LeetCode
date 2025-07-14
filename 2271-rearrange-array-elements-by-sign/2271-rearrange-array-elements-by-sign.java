e71e0bbf0158bf3d3f193ff148bf906d61bab8ab
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-14 14:47:36 +0530

    Time: 3 ms (100%), Space: 77.4 MB (38.49%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,27 +1,47 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length;
+        // Brute Force
+        // int n = nums.length;
 
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        int i = 0;
-        int j = 0;
+        // int[] pos = new int[n/2];
+        // int[] neg = new int[n/2];
+        // int i = 0;
+        // int j = 0;
+        // for(int val : nums){
+        //     if(val > 0){
+        //         pos[i] = val;
+        //         i++;
+        //     }
+        //     else{
+        //         neg[j] = val;
+        //         j++;
+        //     }
+        // }
+
+        // for(int p = 0;p < n / 2;p++){
+        //     nums[2*p] = pos[p];
+        //     nums[2*p + 1] = neg[p];
+        // }
+
+        // return nums;
+
+        // Optimal
+        int n = nums.length;
+        int[] ans = new int[n];
+        int pos = 0;
+        int neg = 1;
+
         for(int val : nums){
             if(val > 0){
-                pos[i] = val;
-                i++;
+                ans[pos] = val;
+                pos += 2;
             }
             else{
-                neg[j] = val;
-                j++;
+                ans[neg] = val;
+                neg += 2; 
             }
         }
 
-        for(int p = 0;p < n / 2;p++){
-            nums[2*p] = pos[p];
-            nums[2*p + 1] = neg[p];
-        }
-
-        return nums;
+        return ans;
     }
 }
