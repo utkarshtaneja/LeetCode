f7f279a529cc1675c6162a75f918d846f8ce86ef
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-16 12:15:59 +0530

    Time: 1091 ms (15.94%), Space: 46.5 MB (65.59%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,31 +1,33 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         // Brute Force
-        // int n = nums.length;
         // int count = 0;
-        // for(int i = 0;i<n;i++){
-        //     int sum = 0;
-        //     for(int j = i;j < n;j++){
-        //         sum = sum + nums[j];
-        //         if(sum == k){
-        //             count++;
+        // int n = nums.length;
+
+        // for (int i = 0;i < n;i++) {
+        //     for (int j = i;j < n;j++) {
+        //         int sum = 0;
+        //         for (int p = i;p <= j;p++) {
+        //             sum += nums[p];
         //         }
+        //         if (sum == k) count++;
         //     }
         // }
-        // return count;
-        
-        int n = nums.length;
-        int count = 0;
-        int prefixSum = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0, 1); 
-        for(int i = 0; i < n; i++) {
-            prefixSum += nums[i];
-            int remove = prefixSum - k;
 
-            count = count + map.getOrDefault(remove, 0);
-            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        // return count;
+
+        // Better
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0;i < n;i++) {
+            int sum = 0;
+            for (int j = i;j < n;j++) {
+                sum += nums[j];
+                if (sum == k) count++;
+            }
         }
+
         return count;
     }
 }
