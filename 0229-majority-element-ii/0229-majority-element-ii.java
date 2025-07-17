5e4beb9281a7c8b6e07160126680b1012e065c66
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-17 12:56:06 +0530

    Time: 15 ms (26.04%), Space: 49.6 MB (89.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        if (nums.length == 0) return ans;
         int n = nums.length;
-        for(int i = 0;i < n;i++){
-            if(map.containsKey(nums[i])){
-                int incr = map.getOrDefault(nums[i],0)+1;
-                map.put(nums[i],incr);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0;i < n;i++) {
+            if (map.containsKey(nums[i])) {
+                int incr = map.getOrDefault(nums[i] , 0) + 1;
+                map.put(nums[i], incr);
             }
-            else{
-                map.put(nums[i],1);
-            }
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue() > n/3){
-                list.add(e.getKey());
+            else {
+                map.put(nums[i], 1);
             }
         }
-        return list;
+
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (e.getValue() > n / 3) {
+                ans.add(e.getKey());
+            }
+        }
+        return ans;
     }
 }
