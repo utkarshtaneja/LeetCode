7e60e23132af9aaf0a3ab4355e566312da3ff9e6
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-17 19:40:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (89.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,45 +1,29 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Brute Force
-        // ArrayList<Integer> list = new ArrayList<>();
-        // for(int i = 0;i < m;i++){
-        //     if(nums1[i] != 0){
-        //         list.add(nums1[i]);
-        //     }
-        // }
-        // for(int i = 0;i < n;i++){
-        //     list.add(nums2[i]);
-        // }
-        // Collections.sort(list);
-        // for(int i = 0;i < list.size();i++){
-        //     nums1[i] = list.get(i);
-        // }
-        
-        // Optimal
-        int[] merge = new int[m + n];
         int i = 0;
         int j = 0;
         int k = 0;
-        
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                merge[k++] = nums1[i++];
+
+        int[] merged = new int[n + m];
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
             }
-            else{
-                merge[k++] = nums2[j++];
+            else {
+                merged[k++] = nums2[j++];
             }
         }
-        
-        while(i < m){
-            merge[k++] = nums1[i++];
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
         }
-        
-        while(j < n){
-            merge[k++] = nums2[j++];
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
         }
-        
-        for(int p = 0;p < m + n;p++){
-            nums1[p] = merge[p];
+
+        for (int p = 0;p < merged.length;p++) {
+            nums1[p] = merged[p];
         }
     }
 }
