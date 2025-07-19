49b2dd85bbdfee521e84f26fd89bbc1cf8eb9aa0
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-19 12:18:32 +0530

    Time: 8 ms (85.98%), Space: 46.8 MB (39.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,56 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        int[] newInterval = intervals[0];
-        List<int[]> ans = new ArrayList<>();
-        ans.add(newInterval);
+        // Brute Force
+        // int n = intervals.length;
+        // Arrays.sort(intervals, new Comparator<int[]>() {
+        //     public int compare(int[] a, int[] b) {
+        //         return a[0] - b[0];
+        //     }
+        // });
         
-        for(int[] x : intervals){
-            if(x[0] <= newInterval[1]){
-                newInterval[1] = Math.max(x[1], newInterval[1]);
+        // List<List<Integer>> ans = new ArrayList<>();
+        // for (int i = 0;i < n;i++) {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+
+        //     if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+        //         continue;
+        //     }
+
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (intervals[j][0] <= end) {
+        //             end = Math.max(end, intervals[j][1]);
+        //         } else {
+        //             break;
+        //         }
+        //     }
+        //     ans.add(Arrays.asList(start, end));
+        // }
+        // int[][] result = new int[ans.size()][2];
+        // for (int i = 0;i < ans.size();i++) {
+        //     result[i][0] = ans.get(i).get(0);
+        //     result[i][1] = ans.get(i).get(1);
+        // }
+        // return result;
+
+        // Optimal
+        int n = intervals.length;
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
             }
-            else{
-                newInterval = x;
-                ans.add(newInterval);
+        });
+        
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } 
+            else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
-        return ans.toArray(new int[ans.size()][]);
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
