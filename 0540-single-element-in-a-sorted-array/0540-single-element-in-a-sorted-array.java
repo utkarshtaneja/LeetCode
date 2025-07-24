29ad59b8dd5e07797242ca393392345f1b00874f
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-24 13:30:50 +0530

    Time: 0 ms (100%), Space: 50.6 MB (29.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,45 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // Brute Force
-        // int ans = 0;
-        // for(int i = 0;i < nums.length;i++){
-        //     ans = ans ^ nums[i];
-        // }
-        // return ans;
-        
-        // Optimal Approach        
-        int n = nums.length;       
-        if(n == 1){
-            return nums[0];
-        }
-        
-        if(nums[0] != nums[1]){
-            return nums[0];
-        }
-        
-        if(nums[n-1] != nums[n-2]){
-            return nums[n-1];
-        }
-        
-        int low = 1;
-        int high = n-2;
-        
-        while(low <= high){
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        int ans = -1;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
-                return nums[mid];
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                ans = nums[mid];
+                break;
             }
-            else if( (mid % 2 == 1 && nums[mid-1] == nums[mid] ) || 
-                     (mid % 2 == 0 && nums[mid] == nums[mid+1] ) ){
+
+            if ( (mid % 2 == 1 && nums[mid - 1] == nums[mid]) || 
+                (mid % 2 == 0 && nums[mid + 1] == nums[mid]) ) {
                 low = mid + 1;
             }
-            else{
+            else {
                 high = mid - 1;
             }
         }
-        return -1;
+        return ans;
     }
 }
-// Play on even and odd index
-// (even, odd) -> left half    ---> element is on right half
-// (odd, even) -> right half   ---> element is on left half
