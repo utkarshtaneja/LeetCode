3ba0d2648dcfa29a5bd0ebd0c5fd2cbd696bb9c3
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-25 14:06:49 +0530

    Time: 24 ms (40.33%), Space: 44.7 MB (98.12%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static int getRequiredTime(int[] piles, int k) {
+        int time = 0;
+        for (int i = 0;i < piles.length;i++) {
+            time += Math.ceil((piles[i] + k - 1) / k);
+        }
+        return time;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        // Brute Force
+        // int maxPile = 0;
+        // for (int pile : piles) {
+        //     maxPile = Math.max(maxPile, pile);
+        // }
+
+        // for (int k = 1; k <= maxPile; k++) {
+        //     int requiredTime = getRequiredTime(piles, k);
+        //     if (requiredTime <= h) {
+        //         return k;
+        //     }
+        // }
+        // return -1;
+
+        // Optimal
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            high = Math.max(high, pile);
+        }
+
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int requiredTime = getRequiredTime(piles, mid);
+
+            if (requiredTime <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
