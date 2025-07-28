14325fc389936d4945852d688cdfbd1fdf7a55ed
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-28 13:13:07 +0530

    Time: 11 ms (76.27%), Space: 47.1 MB (84.47%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,57 @@
 class Solution {
-    public static int findDays(int[] weights,int capacity){
-        int days = 1; 
+    public static int noOfDays(int[] arr, int capacity) {
+        int day = 1;
         int load = 0;
-        int n = weights.length; 
-        for (int i = 0; i < n; i++) {
-            if (load + weights[i] > capacity) {
-                days++; 
-                load = weights[i]; 
-            } 
+
+        for (int i = 0;i < arr.length;i++) {
+            if (load + arr[i] > capacity) {
+                day++;
+                load = arr[i];
+            }
             else {
-                load += weights[i];
+                load += arr[i];
             }
         }
-        return days;
+        return day;
     }
     public int shipWithinDays(int[] weights, int days) {
-        int low = 0;
-        int high = 0;
-        for (int i = 0; i < weights.length; i++) {
-            high = high + weights[i];   // summation
-            low = Math.max(low, weights[i]);    // max value
-        }
+        // Brute Force
+        // int mini = Integer.MIN_VALUE;
+        // int maxi = 0;
 
-        // Range = (maxValue , summation)
+        // for (int weight : weights) {
+        //     mini = Math.max(mini, weight);
+        //     maxi += weight;
+        // }
+
+        // for (int i = mini;i <= maxi;i++) {
+        //     int daysRequired = noOfDays(weights, i);
+        //     if (daysRequired <= days) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Optimal
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+
+        for (int weight : weights) {
+            low = Math.max(low, weight);
+            high += weight;
+        }
+        int ans = -1;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            int numberOfDays = findDays(weights, mid);
-            if (numberOfDays <= days) {
-                //eliminate right half
+            int mid = low + (high - low) / 2;
+            int daysRequired = noOfDays(weights, mid);
+            if (daysRequired <= days) {
+                ans = mid;
                 high = mid - 1;
-            } 
+            }
             else {
-                //eliminate left half
                 low = mid + 1;
             }
         }
-        return low;
-
-        
-    }
+        return ans;
+    }   
 }
