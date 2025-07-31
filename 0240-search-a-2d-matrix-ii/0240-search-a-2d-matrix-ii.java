2369296df754c185d88a755916422319ecd20fca
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-07-31 14:40:32 +0530

    Time: 6 ms (48.7%), Space: 46.4 MB (11.46%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,26 +1,56 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+    public static boolean find(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
 
-        int rowIndex = 0;
-        int colIndex = col - 1;
-        boolean flag = false;
-
-        while(rowIndex < row && colIndex >= 0){
-            int element = matrix[rowIndex][colIndex];
-
-            if(element == target){
-                flag = true;
-                break;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) return true;
+            else if (arr[mid] > target) {
+                high = mid - 1;
             }
-            else if(element < target){
-                rowIndex++;
-            }
-            else{
-                colIndex--;
+            else {
+                low = mid + 1;
             }
         }
-        return flag;
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Brute Force
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+
+        // for (int i = 0;i < n;i++) {
+        //     for (int j = 0;j < m;j++) {
+        //         if (matrix[i][j] == target) return true;
+        //     }
+        // }
+        // return false;
+
+        // Better
+        // int n = matrix.length;
+
+        // for (int i = 0;i < n;i++) {
+        //     if (find(matrix[i], target)) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // Optimal
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) col--;
+            else{
+                row++;
+            }
+        }
+        return false;
     }
 }
