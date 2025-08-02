e9b37a9e0199c3ebba0b32a04dc965714e6d1224
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-02 12:36:59 +0530

    Time: 10 ms (74.66%), Space: 42.6 MB (52.24%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,27 +3,24 @@ class Solution {
         int n1 = s.length();
         int n2 = t.length();
 
-        if(n1 != n2) return false;
+        if (n1 != n2) return false;
+        Map<Character, Character> map = new HashMap<>();
 
-        HashMap<Character, Character> map = new HashMap<>();
-
-        for(int i = 0;i < n1;i++){
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-
-            if(map.containsKey(ch1)){
-                if(map.get(ch1) != ch2){
-                    return false;
-                }
+        for (int i = 0;i < n1;i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map.containsKey(c1)) {
+                if (map.get(c1) != c2) return false;
             }
-            else{
-                if(map.containsValue(ch2)){
+            else {
+                if (map.containsValue(c2)) {
                     return false;
                 }
-                map.put(ch1, ch2);
+                else {
+                    map.put(c1, c2);
+                }
             }
         }
-
         return true;
     }
 }
