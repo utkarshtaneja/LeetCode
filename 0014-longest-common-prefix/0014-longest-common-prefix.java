c609b6b69c9057391a11f360b0c854fd351f164e
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-02 12:25:50 +0530

    Time: 1 ms (62.99%), Space: 41.7 MB (51.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,55 +1,29 @@
-// class Solution {
-//     public String longestCommonPrefix(String[] strs) {
-//         if (strs == null || strs.length == 0) {
-//             return "";
-//         }
-        
-//         String prefix = strs[0]; 
-        
-//         for (int i = 1; i < strs.length; i++) {
-//             while (strs[i].indexOf(prefix) != 0) {
-//                 prefix = prefix.substring(0, prefix.length() - 1);
-//                 if (prefix.isEmpty()) {
-//                     return "";
-//                 }
-//             }
-//         }
-        
-//         return prefix;
-//     }
-// }
+class Solution {
+    public static String compare(String word1, String word2) {
+        int n = Math.min(word1.length(), word2.length());
+        StringBuilder result = new StringBuilder();
 
-// MY SOLUTION
-class Solution{
-    public static String compare(String a,String b){
-        int len = Math.min(a.length(),b.length());
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0;i<len;i++){
-            if(a.charAt(i) != b.charAt(i)){
+        for (int i = 0; i < n; i++) {
+            if (word1.charAt(i) == word2.charAt(i)) {
+                result.append(word1.charAt(i));
+            } 
+            else {
                 break;
             }
-            else{
-                ans.append(a.charAt(i));
-            }
         }
-        return ans.toString();
+
+        return result.toString();
     }
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            prefix = compare(prefix, strs[i]);
+            if (prefix.isEmpty()) break;
         }
-        
-        String temp = strs[0];
-        for(int i = 1;i<strs.length;i++){
-            String s = strs[i];
-            String get = compare(temp,s);
-            if(get == ""){
-                return "";
-            }
-            else{
-                temp = get;
-            }
-        }
-        return temp;
+
+        return prefix;
     }
 }
