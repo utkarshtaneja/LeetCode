7c3f5c97994bfd477f26732074954943dcfa23aa
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-05 13:25:32 +0530

    Time: 1 ms (100%), Space: 42.6 MB (33.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,40 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s == null){
-            return 0;
+        int i = 0, n = s.length();
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
         }
 
-        s = s.trim();
-        if(s.length() == 0){
-            return 0;
-        }
-        
-        int sign = +1;
-        long ans = 0;
-        if(s.charAt(0) == '-'){
+        if (i == n) return 0;
+
+        int sign = 1;
+        if (s.charAt(i) == '-') {
             sign = -1;
+            i++;
+        } 
+        else if (s.charAt(i) == '+') {
+            i++;
         }
-        
-        int MAX = Integer.MAX_VALUE;
-        int MIN = Integer.MIN_VALUE;
-        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i)) ){
-                break;
-            }
-            ans = ans * 10 + s.charAt(i) - '0';
-            // check the conditions
-            if(sign == -1 && -1 * ans < MIN){
-                return MIN;
-            }
-            if(sign == 1 && ans > MAX){
-                return MAX;
-            }
+
+        long ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            ans = ans * 10 + (s.charAt(i) - '0');
+            
+            if (sign == 1 && ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign == -1 && -ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
             
             i++;
         }
-        
-        return (int)(sign*ans);
+
+        return (int)(sign * ans);
     }
 }
