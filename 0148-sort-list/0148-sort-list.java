bfaa98a0982e4725d9c1d80fd46904c4c1d21f7a
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-08 12:39:39 +0530

    Time: 9 ms (96.14%), Space: 56.8 MB (60.54%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,24 +10,66 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode curr = head;
+        // Brute Force
+        // List<Integer> list = new ArrayList<>();
+        // ListNode curr = head;
 
-        while (curr != null) {
-            list.add(curr.val);
-            curr = curr.next;
+        // while (curr != null) {
+        //     list.add(curr.val);
+        //     curr = curr.next;
+        // }
+
+        // curr = head;
+        // Collections.sort(list);
+        // int index = 0;
+
+        // while (curr != null) {
+        //     curr.val = list.get(index);
+        //     curr = curr.next;
+        //     index++;
+        // }
+
+        // return head;
+
+        // Optimal
+        if (head == null || head.next == null) return head; 
+
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+
+        return merge(leftSorted, rightSorted);
+    }
+    public static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } 
+            else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
         }
 
-        curr = head;
-        Collections.sort(list);
-        int index = 0;
+        tail.next = (l1 != null) ? l1 : l2;
 
-        while (curr != null) {
-            curr.val = list.get(index);
-            curr = curr.next;
-            index++;
-        }
-
-        return head;
+        return dummy.next;
     }
 }
