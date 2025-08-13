afa2663f6cac7ead75b06f115aae2f7e08e14615
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-13 12:44:34 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.05%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,40 @@
 class Solution {
+    public static double power(double x, long n) {
+        if (n == 0) return 1.0; 
+        double half = power(x, n / 2);
+        
+        if (n % 2 == 0) {
+            return half * half;
+        } 
+        else {
+            return half * half * x;
+        }
+    }
     public double myPow(double x, int n) {
         // Without Recursion
-        long e = n;
-        while (e < 0) {
-            x = 1/ x;
-            e = -e;
-        }
-        double ans = 1.0;
+        // long e = n;
+        // while (e < 0) {
+        //     x = 1/ x;
+        //     e = -e;
+        // }
+        // double ans = 1.0;
 
-        while (e > 0) {
-            if((e & 1) == 1) {
-                ans *= x;
-            }
-            x *= x;
-            e = e >> 1;
-        }
+        // while (e > 0) {
+        //     if((e & 1) == 1) {
+        //         ans *= x;
+        //     }
+        //     x *= x;
+        //     e = e >> 1;
+        // }
 
-        return ans;
+        // return ans;
+
+        // With recursion
+        long exp = n;
+        if (n < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        return power(x, exp);
     }
 }
