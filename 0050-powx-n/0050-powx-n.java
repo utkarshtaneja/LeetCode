678eee6467bfa968701a2c8f3eed49da8180e981
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-08-13 12:42:16 +0530

    Time: 0 ms (100%), Space: 42.4 MB (46.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,21 @@
 class Solution {
-    public static double pow(double x,int n){
-        // base case
-        if(n == 0){
-            return 1;
-        }
-
-        double half = pow(x, n / 2);
-        
-        if (n % 2 == 0) {
-            return half * half;
-        } 
-        else {
-            return half * half * x;
-        }
-    }
     public double myPow(double x, int n) {
-        // base case
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
+        // Without Recursion
+        long e = n;
+        while (e < 0) {
+            x = 1/ x;
+            e = -e;
+        }
+        double ans = 1.0;
+
+        while (e > 0) {
+            if((e & 1) == 1) {
+                ans *= x;
+            }
+            x *= x;
+            e = e >> 1;
         }
 
-        return pow(x, n);
+        return ans;
     }
 }
